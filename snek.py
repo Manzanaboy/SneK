9a67aa45027f450014919261f8a1fa9b36270129
chevre sama <damian-sebastian.przgrthr@eduge.ch>
2017-04-26 11:37:40 +0200

    Update snek.py

--- snek.py
+++ snek.py
@@ -3,3 +3,13 @@ from Tkinter import *
 ##################################
 ##          fonctions           ##
 ##################################
+
+
+##################################
+fen1 = Tk()
+can = Canvas(fen1,bg="black",height=500,width=500)
+can.pack(side=LEFT)
+bou1 = Button(fen1,text='Quitter',command=fen1.quit)
+bou1.pack(side=BOTTOM)
+fen1.mainloop()
+fen1.destroy()
